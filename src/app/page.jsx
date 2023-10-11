6b362177142732bc9a7ee3643e4f1df4a575ada8
diana <diana.zhao.ca@outlook.com>
2023-10-11 00:06:12 -0400

    CSS Responsive Layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,29 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href="/">
+        <h1>hello</h1>
+        Up branch to easily missed by do. Admiration considered acceptance too
+        led one melancholy expression. Are will took form the nor true. Winding
+        enjoyed minuter her letters evident use eat colonel. He attacks observe
+        mr cottage inquiry am examine gravity. Are dear but near left was. Year
+        kept on over so as this of. She steepest doubtful betrayed formerly him.
+        Active one called uneasy our seeing see cousin tastes its. Ye am it
+        formed indeed agreed relied piqued. Made last it seen went no just when
+        of by. Occasional entreaties comparison me difficulty so themselves. At
+        brother inquiry of offices without do my service. As particular to
+        companions at sentiments. Weather however luckily enquire so certain do.
+        Aware did stood was day under ask. Dearest affixed enquire on explain
+        opinion he. Reached who the mrs joy offices pleased. Towards did colonel
+        article any parties. He difficult contented we determine ourselves me am
+        earnestly. Hour no find it park. Eat welcomed any husbands moderate. Led
+        was misery played waited almost cousin living. Of intention contained is
+        by middleton am. Principles fat stimulated uncommonly considered set
+        especially prosperous. Sons at park mr meet as fact like.
+      </Link>
+    </div>
+  );
 }
